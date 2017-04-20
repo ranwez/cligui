d9c2ff80b0f53c249cc2d1823aab5c4058c9416f
Cédric CAMBON <cedric.cambon@supagro.fr>
2017-04-20 09:17:54 +0200

    Ajout d'un "test" exécutable :
    Création de la classe WindowTest qui permet d'afficher une fenêtre
    (avant, il fallait générer un JAR et tester le JAR sur MACSE)

---
+++ tests/gui/WindowTest.java
@@ -0,0 +1,20 @@
+package gui;
+
+import _cli.CLI_apiTest;
+import cli.CLI_api;
+import cli.CLI_window;
+import data.Bill;
+
+public final class WindowTest
+{
+	public static void main(String[] args) throws Exception
+	{
+		CLI_api api = new CLI_api(CLI_apiTest.class, "files/tests.properties", "prog");
+
+		api.addProgram("bill", Bill.class);
+
+		new CLI_window(api);
+
+		//api.parse("-prog bill -id 4 -name table -price 42.3 -currency £ -credit");
+	}
+}
